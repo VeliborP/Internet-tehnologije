fa12b825e36569ce6830092e28eb4c976b5f1d16
premevskiv <velci.aspire@gmail.com>
2024-11-15 14:43:41 +0100

    Inicijalna postavka MVC projekta
    - WebShop2

---
+++ WebShop2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebShop2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
